a10bf9a8dbed5a655c0efe64e3e5840614979979
leang007 <lanks_ae@hotmail.com>
2018-11-20 21:11:14 +0700

    Line 19 and 20 had change

--- TimeGreeting.c
+++ TimeGreeting.c
@@ -16,10 +16,10 @@ void main()
   timer = time(NULL); //can send null pointer 
   time_now = localtime(&timer); //takes timer and makes in current time zone
   the_time = asctime(time_now); //puts time in text form
-  printf("System Date and Time: %s \n",the_time);
-  printf("Date %2d/%2d/%4d. Time is %2d:%2d\n",
+  printf("Hello. System Date and Time: %s \n",the_time);
+  printf("GoodBye. %2d/%2d/%4d. Time is %2d:%2d\n",
        time_now->tm_mday, time_now->tm_mon, time_now->tm_year + 1900, 
-       time_now->tm_hour, time_now->tm_min);
+       time_now->tm_hour, time_now->tm_msin);
   do
   {
     printf("What is your name? "); scanf("%s", Name);
